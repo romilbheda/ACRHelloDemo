851d149bebe6431956af5e22b59568b40ae1eb9e
romilbheda <romilbheda@hotmail.com>
2019-05-11 11:06:20 +0000

    Project Added

---
+++ ACRHelloDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACRHelloDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var registryURL = Environment.GetEnvironmentVariable("DOCKER_REGISTRY");
+                ViewData["REGISTRYURL"] = registryURL;
+                if (registryURL != "<acrName>.azurecr.io")
+                {
+                    var hostEntry = await System.Net.Dns.GetHostEntryAsync(registryURL);
+                    ViewData["HOSTENTRY"] = hostEntry.HostName;
+
+                    string region = hostEntry.HostName.Split('.')[1];
+                    ViewData["REGION"] = region;
+
+                    var registryIp = System.Net.Dns.GetHostAddresses(registryURL)[0].ToString();
+                    ViewData["REGISTRYIP"] = registryIp;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return View();
+        }
+    }
+}
